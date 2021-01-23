b08e25a41d05fc0838fc1e164c013787a677e053
Dennis Mo <dennismoyunfei@gmail.com>
2021-01-23 12:44:08 -0800

    bug fixes for nice number

--- nice_numbers.py
+++ nice_numbers.py
@@ -6,12 +6,12 @@ Good numbers can be 1+3 or 1+ 9 or so on
 Find the smallest good number  greater than n
 """
 def nice_number(n, m):
-    k, s = 0, 0
+    k, s = -1, 0
     while s < n:
-        s += m^k
         k += 1
+        s += m^k
     
     for i in reversed(range(k)):
         if s - i^m > n:
-            s - i^m
+            s -= i^m
     return s
